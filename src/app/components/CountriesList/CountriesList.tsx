0ab05d95a2460845694e7550c5aed84bef3a288b
babohorquez10 <ba.bohorquez10@uniandes.edu.co>
2023-07-04 21:55:39 -0500

    Countries filters by name and region.

--- src/app/components/CountriesList/CountriesList.tsx
+++ src/app/components/CountriesList/CountriesList.tsx
@@ -7,6 +7,8 @@ import { Country } from "@/models/interfaces/country.interface";
 export default function CountriesList() {
   const [loading, setLoading] = useState(true);
   const [countries, setCountries] = useState<Country[]>([]);
+  const [search, setSearch] = useState("");
+  const [regionFilter, setRegionFilter] = useState("");
 
   useEffect(() => {
     fetch(
@@ -22,12 +24,40 @@ export default function CountriesList() {
 
   if (loading) return <section>Loading...</section>;
 
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.common.toLowerCase().includes(search.trim().toLowerCase()) &&
+      country.region
+        .toLowerCase()
+        .includes(regionFilter.trim().toLocaleLowerCase())
+  );
+
   return (
     <section>
+      <div className="mb-16 flex justify-between">
+        <input
+          className="py-4 px-10 rounded-md bg-white drop-shadow"
+          placeholder="Search for a country..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <select
+          className="py-4 px-10 rounded-md bg-white drop-shadow"
+          value={regionFilter}
+          onChange={(e) => setRegionFilter(e.target.value)}
+        >
+          <option value="">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
       <div className="grid grid-cols-4 gap-16">
-        {countries.map((country) => (
-          <div key={country.cca2}>
-            <CountryCard country={country} />
+        {filteredCountries.map((item) => (
+          <div key={item.cca2}>
+            <CountryCard country={item} />
           </div>
         ))}
       </div>
